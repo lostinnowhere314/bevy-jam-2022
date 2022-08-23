bf99ba9eeafd2e04b3c3086688686351083f3600
lostinnowhere314 <quinlan.leishman@gmail.com>
2022-08-22 23:08:54 -0600

    rename assets-pdn to assets-dev

--- scripts/update_spritesheets.py
+++ scripts/update_spritesheets.py
@@ -5,7 +5,7 @@ import os
 
 # TODO script that goes through and manually updates things we're working on as .pdn files
 
-def update(pdn_file, dest_file):
+def update_pdn_spritesheet(pdn_file, dest_file):
     
     
     image = pypdn.read(pdn_file)
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     # Figure out filepaths
     cargo_root = pathlib.Path(__file__).parent.parent.resolve()
     
-    pdn_folder = os.path.join(cargo_root, 'assets-pdn')
+    pdn_folder = os.path.join(cargo_root, 'assets-dev')
     assets = os.path.join(cargo_root, 'assets')
 
     # Go through the images
@@ -30,7 +30,9 @@ if __name__ == '__main__':
     ]
     
     for source, dest in items:
-        update(
+        update_pdn_spritesheet(
             os.path.join(pdn_folder,source), 
             os.path.join(assets,dest)
         )
+    
+    # Update UI items
