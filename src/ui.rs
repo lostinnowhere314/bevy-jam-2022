fb338987e1058c334703b5e0ba4235970682a665
lostinnowhere314 <quinlan.leishman@gmail.com>
2022-08-26 00:59:06 -0600

    Finish in-game ui

--- src/ui.rs
+++ src/ui.rs
@@ -6,27 +6,39 @@ pub struct UIPlugin;
 
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(AllMouseoverTargets::new())
+        app
+			.insert_resource(AllMouseoverTargets::new())
             .insert_resource(SpellUiActive(false))
             .insert_resource(CurrentMouseoverTarget(None))
             .add_startup_system(setup_spell_ui)
             .add_system(update_spell_ui_visibility)
             .add_system(toggle_spell_ui)
 			.add_system(update_rune_ui_displays)
+			.add_system(update_spell_selection)
 			.add_system(update_selection_rune_containers.before(update_rune_ui_displays))
+			.add_system(update_inventory_rune_containers.before(update_rune_ui_displays))
+			.add_system(update_queued_rune_containers.before(update_rune_ui_displays))
             .add_system_to_stage(CoreStage::PreUpdate, update_cursor_ui_target);
     }
 }
 
+/// Resource
 /// Keeps track of whether the spell inventory UI is active.
 pub struct SpellUiActive(pub bool);
 
-/// Component for updating ui rune sprites. Recommended to put on a child entity.(probably)
+/// Component for updating ui rune sprites. Recommended to put on a child entity if used with SpellSelectUi
 #[derive(Debug, Component, Deref)]
 pub struct UiRuneContainer(pub Option<spells::Rune>);
 /// Component for updating the above for selected runes
 #[derive(Debug, Component)]
 pub struct SelectedRuneContainer(pub usize);
+// Component to update the above for inventory runes
+#[derive(Debug, Component)]
+pub struct InventoryRuneContainer(pub usize);
+// Component to update the above for queued slots
+#[derive(Debug, Component)]
+pub struct QueuedRuneContainer(pub usize);
+const N_QUEUED_SHOW: usize = 6; //needs to be even to avoid bugs
 /// Resource to store rune sprites
 #[derive(Debug, Deref)]
 pub struct RuneUiSprites(pub HashMap<spells::Rune, Handle<Image>>);
@@ -62,6 +74,44 @@ fn update_selection_rune_containers(
 	}
 }
 
+/// Updates containers based off of the selected runes
+fn update_inventory_rune_containers(
+	mut query: Query<(&InventoryRuneContainer, &mut UiRuneContainer)>,
+	inventory: Res<spells::RuneInventory>
+) {
+	for (index_container, mut rune_container) in query.iter_mut() {
+		let inventory_slot = inventory.0.get(index_container.0).expect("invalid InventoryRuneContainer index encountered");
+		
+		rune_container.0 = if inventory_slot.unlocked {
+			Some(inventory_slot.rune)
+		} else {
+			None
+		}
+	}
+}
+
+/// Updates containers based off of the selected runes
+fn update_queued_rune_containers(
+	mut query: Query<(&QueuedRuneContainer, &mut UiRuneContainer)>,
+	queue_query: Query<&spells::RuneCastQueue, With<player::Player>>
+) {
+	let queue = queue_query.single();
+	
+	for (index_container, mut rune_container) in query.iter_mut() {
+		let show_index = if queue.len() < N_QUEUED_SHOW {
+			index_container.0
+		} else {
+			queue.len() - N_QUEUED_SHOW + index_container.0
+		};
+		
+		rune_container.0 = if let Some(rune) = queue.get(show_index) {
+			Some(*rune)
+		} else {
+			None
+		}
+	}
+}
+
 /// Mark as part of the spell UI. if inventory_page is true, will only show when inventory is open.
 #[derive(Component, Debug)]
 struct SpellSelectUi {
@@ -89,14 +139,14 @@ fn toggle_spell_ui(
     }
 }
 
-const SPELL_SLOT_SIZE: i32 = 20;
-
 fn get_scaled_size(w: i32, h: i32) -> Node {
     Node {
         size: Vec2::new(w as f32, h as f32) * 2.0,
     }
 }
 
+/// Sets up all of the spell ui.
+/// This function is absolutely monolithic
 fn setup_spell_ui(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
@@ -104,7 +154,7 @@ fn setup_spell_ui(
 ) {
     let mut new_mouseover_targets = Vec::<MouseoverTargetSpace>::new();
 
-    let rune_slot_handle = asset_server.load("ui/spell-slot.png");
+    let rune_slot_handle: Handle<Image> = asset_server.load("ui/spell-slot.png");
 
 	// Selected rune slots
 	let spell_slot_file_paths = [
@@ -115,6 +165,11 @@ fn setup_spell_ui(
 		"ui/spell-slot-e.png",
 	];
 	
+	// bunch of positioning constants
+	let selected_row_top = 80.0;
+	let inventory_row_top = 160.0;
+	
+	// Set up rune selection slots ////////////////////////////////
 	for (i, &path) in spell_slot_file_paths.iter().enumerate() {
 		let image_handle: Handle<Image> = asset_server.load(path);
 		
@@ -126,7 +181,7 @@ fn setup_spell_ui(
 					position_type: PositionType::Absolute,
 					position: UiRect {
 						top: Val::Px(4.0),
-						left: Val::Px(640.0 - (4.0 + (4-i + 1) as f32 * 44.0)),
+						left: Val::Px(640.0 - (4.0 + (1+4-i) as f32 * 44.0)),
 						..default()
 					},
 					..default()
@@ -170,30 +225,46 @@ fn setup_spell_ui(
 					.insert(UiRuneContainer(None))
 					.insert(SelectedRuneContainer(i));
 			});
-			
-		/*commands
-			.spawn_bundle(ImageBundle {
-				node: get_scaled_size(20, 26),
+		
+		// When UI is open
+		let selection_slot = commands
+			.spawn_bundle(NodeBundle {
+				node: get_scaled_size(20, 28),
 				style: Style {
 					position_type: PositionType::Absolute,
 					position: UiRect {
-						top: Val::Px(4.0),
-						right: Val::Px(4.0 + (4-i) as f32 * 44.0),
+						top: Val::Px(selected_row_top),
+						// Center them horizontally
+						left: Val::Px(320.0 + (-20.0 + (i as f32 - 2.0) * 44.0)),
 						..default()
 					},
 					..default()
 				},
-				image: UiImage(asset_server.load(path)),
 				..default()
 			})
-			.insert(SpellSelectUi{ inventory_page: false})
+			.insert(SpellSelectUi{ inventory_page: true})
 			.with_children(|parent| {
-				// Inner node for displaying runes
+				// Inner node for actually displaying things
+				parent
+					.spawn_bundle(ImageBundle {
+						node: get_scaled_size(20, 28),
+						style: Style {
+							position_type: PositionType::Absolute,
+							position: UiRect {
+								top: Val::Px(0.0),
+								left: Val::Px(0.0),
+								..default()
+							},
+							..default()
+						},
+						image: UiImage(asset_server.load(path)),
+						..default()
+					});
 				parent
 					.spawn_bundle(ImageBundle {
 						node: get_scaled_size(16, 16),
 						style: Style {
-							position_type: PositionType::Relative,
+							position_type: PositionType::Absolute,
 							position: UiRect {
 								top: Val::Px(4.0),
 								left: Val::Px(4.0),
@@ -207,44 +278,166 @@ fn setup_spell_ui(
 					})
 					.insert(UiRuneContainer(None))
 					.insert(SelectedRuneContainer(i));
-			});*/
+			}).id();
+		// Create a mouseover target for it
+		new_mouseover_targets.push(MouseoverTargetSpace {
+			target: MouseoverTarget::SpellSelectedSlot(i),
+			top: selected_row_top,
+			left: 320.0 + (-20.0 + (2.0-i as f32) * 44.0),
+			width: 20.0 * 2.0,
+			height: 24.0 * 2.0,
+			source_entity: selection_slot,
+		});
 	}
-
-    // TODO actually make the UI we want
-    // Spawn the ui element
-    let entity = commands
-        .spawn_bundle(ImageBundle {
-            node: get_scaled_size(SPELL_SLOT_SIZE, SPELL_SLOT_SIZE),
-            image: rune_slot_handle.into(),
-            style: Style {
-                position_type: PositionType::Absolute,
-                position: UiRect {
-                    top: Val::Px(10.0),
-                    left: Val::Px(10.0),
-                    ..default()
-                },
-                ..default()
-            },
-            ..default()
-        })
-        .insert(SpellSelectUi {
-            inventory_page: true,
-        })
-        .id();
-
-    // Insert its target
-    new_mouseover_targets.push(MouseoverTargetSpace {
-        target: MouseoverTarget::SpellSelectedSlot(0),
-        top: 10.0,
-        left: 10.0,
-        width: SPELL_SLOT_SIZE as f32 * 2.0,
-        height: SPELL_SLOT_SIZE as f32 * 2.0,
-        source_entity: entity,
-    });
-    //... (in a loop probably)
+	
+	// Set up rune inventory slots ////////////////////////////////
+	for col in 0..4 {
+		for row in 0..2 {
+			let idx = col + 4 * row;
+			
+			let inventory_slot = commands
+				.spawn_bundle(NodeBundle {
+					node: get_scaled_size(20, 28),
+					style: Style {
+						position_type: PositionType::Absolute,
+						position: UiRect {
+							top: Val::Px(inventory_row_top + row as f32 * 44.0),
+							// Center them horizontally
+							left: Val::Px(320.0 + 4.0 + (col as f32 - 2.0) * 44.0),
+							..default()
+						},
+						..default()
+					},
+					..default()
+				})
+				.insert(SpellSelectUi{ inventory_page: true})
+				.with_children(|parent| {
+					// Inner node for actually displaying things
+					parent
+						.spawn_bundle(ImageBundle {
+							node: get_scaled_size(20, 20),
+							style: Style {
+								position_type: PositionType::Absolute,
+								position: UiRect {
+									top: Val::Px(0.0),
+									left: Val::Px(0.0),
+									..default()
+								},
+								..default()
+							},
+							image: UiImage(rune_slot_handle.clone()),
+							..default()
+						});
+					parent
+						.spawn_bundle(ImageBundle {
+							node: get_scaled_size(16, 16),
+							style: Style {
+								position_type: PositionType::Absolute,
+								position: UiRect {
+									top: Val::Px(4.0),
+									left: Val::Px(4.0),
+									..default()
+								},
+								..default()
+							},
+							image: UiImage(asset_server.load("no-sprite.png")),
+							visibility: Visibility { is_visible: false },
+							..default()
+						})
+						.insert(UiRuneContainer(None))
+						.insert(InventoryRuneContainer(idx));
+				}).id();
+			// Create a mouseover target for it
+			new_mouseover_targets.push(MouseoverTargetSpace {
+				target: MouseoverTarget::SpellInventorySlot(idx),
+				top: inventory_row_top + row as f32 * 44.0,
+				left: 320.0 + 4.0 + (col as f32 - 2.0) * 44.0,
+				width: 20.0 * 2.0,
+				height: 20.0 * 2.0,
+				source_entity: inventory_slot,
+			});
+		}
+	}
+	
+	// Set up queued spell slots //////////////////////////////////
+	for i in 0..N_QUEUED_SHOW {
+		commands
+			.spawn_bundle(ImageBundle {
+				node: get_scaled_size(16, 16),
+				style: Style {
+					position_type: PositionType::Absolute,
+					position: UiRect {
+						bottom: Val::Px(8.0),
+						left: Val::Px(320.0 + 2.0 + (i as f32 - N_QUEUED_SHOW as f32 / 2.0) * 36.0),
+						..default()
+					},
+					..default()
+				},
+				image: UiImage(asset_server.load("no-sprite.png")),
+				visibility: Visibility { is_visible: false },
+				..default()
+			})
+			.insert(UiRuneContainer(None))
+			.insert(QueuedRuneContainer(i));
+	}
 
     // include all the new targets
     all_mouseover_targets.0.append(&mut new_mouseover_targets);
+}
+
+//// Update spell selection ///////////////////////////////
+fn update_spell_selection(
+	action_query: Query<&ActionState<player::Action>, With<player::Player>>,
+	mouseover_target: Res<CurrentMouseoverTarget>,
+	mut selected_runes: ResMut<spells::EquippedRunes>,
+	rune_inventory: Res<spells::RuneInventory>,
+    spell_ui_active: Res<SpellUiActive>,
+) {
+	if !spell_ui_active.0 {
+		return;
+	}
+	
+	let action_state = action_query.single();
+	
+	let maybe_action_index = {
+		let mut result: Option<usize> = None;
+		
+		for (idx, action) in player::SPELL_COMP_ACTIONS.iter().enumerate() {
+			if action_state.just_pressed(*action) {
+				result = Some(idx);
+			}
+		}
+		result
+	};
+	
+	if let (Some(action_idx), Some(target)) = (maybe_action_index, mouseover_target.0) {
+		match target.0 {
+			MouseoverTarget::SpellSelectedSlot(idx) => {
+				// Swap spells
+				selected_runes.0.swap(action_idx, idx);
+			}
+			MouseoverTarget::SpellInventorySlot(idx) => if let Some(inventory_slot) = rune_inventory.0.get(idx) {
+				if !inventory_slot.unlocked {
+					return;
+				}
+				// Clear it from the selected spells if it's there already
+				for j in 0..5 {
+					if let Some(maybe_rune) = selected_runes.0.get_mut(j) {
+						if let Some(rune) = maybe_rune {
+							if *rune == inventory_slot.rune {
+								*maybe_rune = None;
+							}
+						}
+					}
+				}
+				
+				// Set it to the new rune
+				if let Some(elem) = selected_runes.0.get_mut(action_idx) {
+					*elem = Some(inventory_slot.rune);
+				}
+			}
+		}
+	}
 }
 
 // Resource for storing available targets
@@ -285,8 +478,8 @@ pub struct CurrentMouseoverTarget(pub Option<(MouseoverTarget, Entity)>);
 // Mouse interaction, to determine if we are clicking on UI
 #[derive(Debug, Clone, Copy)]
 pub enum MouseoverTarget {
-    SpellSelectedSlot(u32),
-    SpellInventorySlot(u32),
+    SpellSelectedSlot(usize),
+    SpellInventorySlot(usize),
 }
 
 /// Gets the position of the cursor if in the primary window
