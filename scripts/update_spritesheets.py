c36a9ab95c84bef24558978b790201fc4952ef43
lostinnowhere314 <quinlan.leishman@gmail.com>
2022-08-26 20:09:23 -0600

    health and mana bar sprites

--- scripts/update_spritesheets.py
+++ scripts/update_spritesheets.py
@@ -2,6 +2,7 @@ import pypdn
 import imageio
 import pathlib
 import os
+import re
 from glob import glob
 import numpy as np
 
@@ -38,21 +39,33 @@ def update_pdn_spritesheet(pdn_file, dest_file):
     imageio.imwrite(dest_file, image)
     
 @_progress
-def upscale_ui_element(source_file, dest_file, scale_factor=2):
+def upscale_ui_element(source_file, dest_file, scale_factor=2, split_ct=None):
     src_image = read_png_or_pdn(source_file)
     
+    if split_ct is None:
+        dest_image = upscale_indiv(src_image, scale_factor)
+        imageio.imwrite(dest_file, dest_image)
+    else:
+        split_size = src_image.shape[1] // split_ct
+        
+        for i in range(split_ct):
+            dest_image = upscale_indiv(src_image[:,split_size*i:split_size*(i+1),:], scale_factor)
+            imageio.imwrite(dest_file.format(i), dest_image)
+        
+    
+def upscale_indiv(image, scale_factor):
     dest_shape = (
-        scale_factor * src_image.shape[0],
-        scale_factor * src_image.shape[1],
-        src_image.shape[2]
+        scale_factor * image.shape[0],
+        scale_factor * image.shape[1],
+        image.shape[2]
     )
-    dest_image = np.empty(dest_shape, dtype=src_image.dtype)
+    dest_image = np.empty(dest_shape, dtype=image.dtype)
     
     for i in range(scale_factor):
         for j in range(scale_factor):
-            dest_image[i::scale_factor,j::scale_factor] = src_image
+            dest_image[i::scale_factor,j::scale_factor] = image
+    return dest_image
     
-    imageio.imwrite(dest_file, dest_image)
 
 if __name__ == '__main__':
     # Figure out filepaths
@@ -80,7 +93,18 @@ if __name__ == '__main__':
     
     for source in ui_source_dir:
         source = source.replace('\\','/')
-        end_filen = source.split('ui/')[1].replace('.pdn','.png')
-        dest = os.path.join(assets, 'ui', end_filen)
         
-        upscale_ui_element(source, dest)
+        expr = re.compile(r'/split-([0-9]+)-')
+        match = expr.search(source)
+        if match:
+            end_filen = expr.split(source)[-1].replace('.pdn','.png').replace('.png','-{}.png')
+            dest = os.path.join(assets, 'ui', end_filen)
+            
+            split_ct = int(match.group(1))
+        else:
+            end_filen = source.split('ui/')[1].replace('.pdn','.png')
+            dest = os.path.join(assets, 'ui', end_filen)
+            
+            split_ct = None
+            
+        upscale_ui_element(source, dest, split_ct=split_ct)
